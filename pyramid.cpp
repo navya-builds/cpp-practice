ba4e395d964bb0dd6e7f79fd34f3b80feeaa6b7f
Navya <2k25cse2511181@gmail.com>
2026-06-09 22:52:06 +0530

    Create pyramid.cpp

---
+++ pyramid.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"enter number of rows";
+cin>>n;
+int m;
+cout<<"enter number of columns";
+cin>>m;
+for(int i=0;i<n;i++){
+    for(int j=0;j<n-i-1;j++){
+   cout<<" ";
+    }
+    for(int j=0;j<2*i+1;j++){
+        cout<<"*";
+    }
+    for(int j=0;j<n-i-1;j++){
+        cout<<" ";
+    }
+    cout<<endl;
+}
+
+return 0;
+
+}
